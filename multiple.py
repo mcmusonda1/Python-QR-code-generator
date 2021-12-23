0e64ff111ff22a5930dea86f32e2e8bd7903711e
mcmusonda1 <mcmusondaleonard@gmail.com>
2021-12-23 19:16:21 +0300

    added inputs for course and year plus inline feature for them

--- multiple.py
+++ multiple.py
@@ -6,8 +6,8 @@ email = input("Enter your email address: \n")
 dob = input("Enter your date of birth: \n")
 gender = input("Enter your gender: \n")
 university = input("Enter the name of your university: \n")
-course = ("Enter name of the course your are doing: ")
-year = ("Enter the year of study you are in: ")
+course = input("Enter name of the course your are doing: \n")
+year = input("Enter the year of study you are in: \n")
 
 
 image = qrcode.make('Name: ' + name + ' ' + '\n Phone number: ' + Phone + ' ' + '\n Email: ' + email + ' ' + '\n Date Of Birth: ' + dob + ' ' + '\n Gender: ' + gender + ' ' + '\n University: ' + university + '\n Course: ' + course + ' ' + '\n Year: ' + year)
