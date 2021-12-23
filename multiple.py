333fde68f9de1012ccd6d60a5756e2db1ba856d1
mcmusonda1 <mcmusondaleonard@gmail.com>
2021-12-23 18:59:23 +0300

    Added email input and qr code generator code and an output

--- multiple.py
+++ multiple.py
@@ -1,4 +1,10 @@
 import qrcode
 
 name = input("Enter your name: \n")
-Phone = input("Enter you mobile number: \n")
+Phone = input("Enter your mobile number: \n")
+email = input("Enter your email address: \n")
+
+
+image = qrcode.make('Name: ' + name + ' ' + '\n Phone number: ' + Phone + ' ' + '\n Email: ' + email)
+type(image)
+image.save("Mutiple.png")
